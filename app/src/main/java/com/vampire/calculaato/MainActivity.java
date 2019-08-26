ed63e602bd40fc1e07631fe85ab8fc26b9a176f1
viveksharma7354 <vivek.sharma7354@gmail.com>
2019-08-26 21:25:54 +0530

    Initial Commit

--- app/src/main/java/com/vampire/calculaato/MainActivity.java
+++ app/src/main/java/com/vampire/calculaato/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        public static void main
+        public static void Jav(){}
     }
 }
